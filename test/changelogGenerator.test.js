d43723d036534c8be5ccdf1e89a66e3b8eb04b2b
Sebastian Hiebl <bastidest@mailbox.org>
2022-07-12 21:32:10 +0200

    fix: always include breaking changes in changelog (#96)
    * test: create basic changelogGenerator test cases
    
    * fix: always include breaking changes in changelog
    
    * test: always include breaking changes in changelog
    
    * refactor: move changelog filtering logic to lib/changelogGenerator
    
    * refactor: use pure filtering functions in changelog generation
    
    * refactor: remove unused first/last changelog commit parameters

---
+++ test/changelogGenerator.test.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const path = require("path");
+const temp = require("tmp-promise");
+const execAsync = require("../lib/execAsync");
+
+const commandChangelog = require("../lib/commands/commandChangelog");
+
+beforeEach(async () => {
+  const gitDir = await temp.dir();
+  process.chdir(gitDir.path);
+  await execAsync("git init");
+});
+
+const createSimpleChangelog = async (commitSubject, commitBody) => {
+  const changelogFile = "CHANGELOG.md";
+
+  // add commitMessage 0
+  fs.writeFileSync("text.txt", "0");
+  await execAsync("git add text.txt");
+  await execAsync("git commit -a -m init");
+
+  // add tag 1
+  const givenVersionTag1 = "v1.0.0";
+  await execAsync(`git tag -a -m ${givenVersionTag1} ${givenVersionTag1}`);
+
+  // add commitMessage 1
+  fs.writeFileSync("text.txt", "1");
+
+  let gitCmd = `git commit -a -m "${commitSubject}"`;
+  if (commitBody) {
+    gitCmd += ` -m "${commitBody}"`;
+  }
+
+  await execAsync(gitCmd);
+
+  // WHEN
+  await commandChangelog.handler({
+    config: path.resolve(__dirname, "../git-conventional-commits.default.json"),
+    file: changelogFile,
+  });
+
+  return fs.readFileSync(changelogFile).toString();
+};
+
+test("commandChangelog - included commit", async () => {
+  // GIVEN
+  const commitType = "fix";
+  const commitMessage = "prevent bad stuff from happening";
+  const commitSubject = `${commitType}: ${commitMessage}`;
+
+  // WHEN
+  const changelogString = await createSimpleChangelog(commitSubject, null);
+
+  // THEN
+  expect(changelogString).toMatch(
+    new RegExp(`Bug Fixes\\s+\\*\\s+${commitMessage}`, "g")
+  );
+});
+
+test("commandChangelog - ignored commit", async () => {
+  // GIVEN
+  const commitType = "refactor";
+  const commitMessage = "make all private fields lowercase";
+  const commitSubject = `${commitType}: ${commitMessage}`;
+
+  // WHEN
+  const changelogString = await createSimpleChangelog(commitSubject, null);
+
+  // THEN
+  expect(changelogString).toMatch(/no relevant changes/);
+});
+
+test("commandChangelog - included because breaking", async () => {
+  // GIVEN
+  const commitType = "refactor";
+  const commitMessage = "make all private fields lowercase";
+  const commitSubject = `${commitType}: ${commitMessage}`;
+
+  const commitBodyType = "BREAKING CHANGES";
+  const commitBodyMessage =
+    "configuration field foo is renamed to bar, update your configuration file";
+  const commitBody = `${commitBodyType}: ${commitBodyMessage}`;
+
+  // WHEN
+  const changelogString = await createSimpleChangelog(
+    commitSubject,
+    commitBody
+  );
+
+  // THEN
+  expect(changelogString).toMatch(
+    new RegExp(`BREAKING CHANGES\\s+\\*\\s+${commitBodyMessage}`, "g")
+  );
+});
